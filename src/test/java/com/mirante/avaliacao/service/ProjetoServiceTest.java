79d096bec4c3469fab3063e895d5c6c43366dfd6
Wesley Bruno <wesleybruno910@gmail.com>
2024-05-17 18:05:34 -0300

    Test: cria classe de testes para ProjetoService

---
+++ src/test/java/com/mirante/avaliacao/service/ProjetoServiceTest.java
@@ -0,0 +1,27 @@
+package com.mirante.avaliacao.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ProjetoServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void pesquisarCidades() {
+    }
+
+    @Test
+    void incluirCidade() {
+    }
+
+    @Test
+    void alterarCidade() {
+    }
+
+    @Test
+    void excluirCidade() {
+    }
+}
