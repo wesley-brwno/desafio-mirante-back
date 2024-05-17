334ae6ebab3a7b07dae5b206e39744e3821b2d94
Wesley Bruno <wesleybruno910@gmail.com>
2024-05-17 19:25:54 -0300

    test: Implementa teste para o método pesquisarCidades()
    Esse teste verifica se pesquisarCidades() retorna um List de objetos CidadeDTO quando bem-sucedido

--- src/test/java/com/mirante/avaliacao/service/ProjetoServiceTest.java
+++ src/test/java/com/mirante/avaliacao/service/ProjetoServiceTest.java
@@ -1,17 +1,48 @@
 package com.mirante.avaliacao.service;
 
+import com.mirante.avaliacao.dto.CidadeDTO;
 import com.mirante.avaliacao.model.Cidade;
+import com.mirante.avaliacao.repository.CidadeRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
 class ProjetoServiceTest {
+
+    @Mock
+    CidadeRepository cidadeRepository;
+
+    @InjectMocks
+    ProjetoService projetoService;
+
+    Cidade cidadeValida;
 
     @BeforeEach
     void setUp() {
+        cidadeValida = criaCidadeValida();
     }
 
     @Test
-    void pesquisarCidades() {
+    void pesquisarCidades_DeveRetornarListDeCidadeDTO_QuandoBemSucedido() {
+        BDDMockito.when(cidadeRepository.findAll()).thenReturn(List.of(cidadeValida));
+
+        List<CidadeDTO> cidadeDTOS = projetoService.pesquisarCidades();
+
+        Assertions.assertThat(cidadeDTOS)
+                .isNotNull()
+                .hasSize(1)
+                .isInstanceOf(List.class);
+
+        Assertions.assertThat(cidadeDTOS.get(0))
+                .isEqualTo(criaCidadeDtoValido());
     }
 
     @Test
@@ -33,5 +64,9 @@ class ProjetoServiceTest {
         cidade.setUf("SP");
         cidade.setCapital(true);
         return cidade;
+    }
+
+    private CidadeDTO criaCidadeDtoValido() {
+        return CidadeDTO.toDTO(criaCidadeValida());
     }
 }
