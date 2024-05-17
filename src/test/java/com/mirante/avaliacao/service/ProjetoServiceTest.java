860af6dd1d2f829b5f9547fd797f2b6b7dfa6560
Wesley Bruno <wesleybruno910@gmail.com>
2024-05-17 18:15:53 -0300

    test: cria método factory para entidade Cidade
    Esse método retorna um instância validade de Cidade para ser usados nos testes unitários

--- src/test/java/com/mirante/avaliacao/service/ProjetoServiceTest.java
+++ src/test/java/com/mirante/avaliacao/service/ProjetoServiceTest.java
@@ -1,5 +1,6 @@
 package com.mirante.avaliacao.service;
 
+import com.mirante.avaliacao.model.Cidade;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -23,5 +24,14 @@ class ProjetoServiceTest {
 
     @Test
     void excluirCidade() {
+    }
+
+    private Cidade criaCidadeValida() {
+        Cidade cidade = new Cidade();
+        cidade.setId(1L);
+        cidade.setNome("São Paulo");
+        cidade.setUf("SP");
+        cidade.setCapital(true);
+        return cidade;
     }
 }
