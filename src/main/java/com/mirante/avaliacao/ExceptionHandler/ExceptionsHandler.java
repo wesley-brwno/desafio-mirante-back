a8ff333a7c933ac7a0464afedd6359b83dd1b61e
Wesley Bruno <wesleybruno910@gmail.com>
2024-05-18 18:46:26 -0300

    feat: Implementa o método handleIllegalArgumentException()
    Retorna o objeto MensagemDeError e o status HTTP 400 Bad Request na resposta

--- src/main/java/com/mirante/avaliacao/ExceptionHandler/ExceptionsHandler.java
+++ src/main/java/com/mirante/avaliacao/ExceptionHandler/ExceptionsHandler.java
@@ -17,6 +17,12 @@ public class ExceptionsHandler {
                 .body(criaMensagemDeError(ex, HttpStatus.NOT_FOUND, "Not Found"));
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<MensagemDeError> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(criaMensagemDeError(ex, HttpStatus.BAD_REQUEST, "Bad Request"));
+    }
+
     private MensagemDeError criaMensagemDeError(Exception ex, HttpStatus status, String titulo) {
         return MensagemDeError.builder()
                 .titulo(titulo)
