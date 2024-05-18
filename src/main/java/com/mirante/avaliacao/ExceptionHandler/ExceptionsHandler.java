36f56cff980466ff93e001e9735f5288b1564d20
Wesley Bruno <wesleybruno910@gmail.com>
2024-05-18 18:04:30 -0300

    feat: Adiciona classe ExceptionsHandler e implementa o método handleEntityNotFoundException()

---
+++ src/main/java/com/mirante/avaliacao/ExceptionHandler/ExceptionsHandler.java
@@ -0,0 +1,28 @@
+package com.mirante.avaliacao.ExceptionHandler;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class ExceptionsHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<MensagemDeError> handleEntityNotFoundException(EntityNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(criaMensagemDeError(ex, HttpStatus.NOT_FOUND, "Not Found"));
+    }
+
+    private MensagemDeError criaMensagemDeError(Exception ex, HttpStatus status, String titulo) {
+        return MensagemDeError.builder()
+                .titulo(titulo)
+                .mensagem(ex.getMessage())
+                .timestamp(LocalDateTime.now())
+                .status(status.value())
+                .build();
+    }
+}
