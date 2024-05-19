6785951366b71d9c2582def3579d1fc39a01d9dc
Wesley Bruno <wesleybruno910@gmail.com>
2024-05-19 18:44:14 -0300

    refactor: Renomeação e Reorganização da Classe de Testes
     A classe de testes DemoApplicationTests foi movida do pacote `com.example.demo` para `com.mirante.avaliacao`, e o nome da classe foi alterado de DemoApplicationTests para ProjetoApiApplicationTest

--- src/test/java/com/mirante/avaliacao/ProjetoApiApplicationTest.java
+++ src/test/java/com/mirante/avaliacao/ProjetoApiApplicationTest.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.mirante.avaliacao;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ProjetoApiApplicationTest {
 
 	@Test
 	void contextLoads() {
